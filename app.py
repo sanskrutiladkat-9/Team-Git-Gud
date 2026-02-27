dd920e42508c34fde5f6c4ada6c7658e434c0034
sanskrutiladkat-9 <sanskruti.ladkat@gmail.com>
2026-02-27 17:41:38 +0530

    Update app.py

--- app.py
+++ app.py
@@ -174,7 +174,7 @@ st.markdown("""
         
 st.markdown("---")
 with st.sidebar:
-    st.image("https://drive.google.com/file/d/1Jyo6Yr66VcFplX5_CHjcxzw6TJ5Ljojt/view?usp=sharing", width=150)
+    st.image("st.image("https://drive.google.com/uc?export=view&id=1Jyo6Yr66VcFpIX5_CHjcxzw6TJ5LjojT", width=150)", width=150)
     st.title("🌿 MindNest")
     st.markdown("### Anonymous Support System")
     st.write("Step-Based Mental Health Assistance")
